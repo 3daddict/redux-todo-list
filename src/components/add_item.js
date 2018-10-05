d6b98b77aa4a24eab63147a443008afd822783f2
Michael Salvati <print@3daddict.com>
2018-10-05 11:37:43 -0700

    added data validation to add item

--- src/components/add_item.js
+++ src/components/add_item.js
@@ -3,12 +3,14 @@ import { Field, reduxForm } from 'redux-form';
 
 class AddItem extends Component {
 
-    renderInput(props){
+    renderInput({ input, label, meta: { touched, error } }){
+
         return (
             <div className="row">
                 <div className="s12">
-                    <label>Label Here</label>
-                    <input {...props.input} type="text" />
+                    <label>{label}</label>
+                    <input {...input} type="text" autoComplete="off" />
+                    <p className="red-text text-darken-2">{touched && error}</p>
                 </div>
             </div>
         )
@@ -28,8 +30,8 @@ class AddItem extends Component {
                 <div className="row">
                     <div className="col s8 offset-s2">
                         <form onSubmit={handleSubmit(this.saveItem)} >
-                            <Field name="title" component={this.renderInput} />
-                            <Field name="details" component={this.renderInput} />
+                            <Field name="title" component={this.renderInput} label="Title" />
+                            <Field name="details" component={this.renderInput} label="Details" />
                             <div className="row">
                                 <div className="s12 right-align">
                                     <button className="btn blue-grey darken-1">Add Item</button>
@@ -43,6 +45,22 @@ class AddItem extends Component {
     }
 }
 
+function validate({title, details}){
+
+    const errors = {};
+
+    if(!title){
+        errors.title = 'Please give your item a title';
+    }
+
+    if(!details){
+        errors.details = 'Please give your item some details';
+    }
+    
+    return errors;   
+}
+
 export default reduxForm({
-    form: 'add-item'
+    form: 'add-item',
+    validate: validate
 })(AddItem);
